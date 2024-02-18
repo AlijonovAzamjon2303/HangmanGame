39a2031c3cea1f86fb8aeb52344c6a1b25c98585
A'zamjon <alijonovrobo@gmail.com>
2024-02-18 21:23:56 +0500

    Dasturning 1-versiyasi

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿// book
+string correctAnswer = "Book";
+Console.WriteLine("Assalomu alaykum, Hangman o'yiniga xush kelibsiz!");
+Console.WriteLine("Sizga savol beriladi va siz ularga javob topish orqali");
+Console.WriteLine("yashiringan so'zni topishingiz kerak bo'ladi\n");
+
+Console.WriteLine("\"----\" ko'rinishidagi so'zni topishingiz kerak bo'ladi");
+Console.WriteLine("Qani unda boshladik!\n");
+Thread.Sleep(3000);
+
+string word = "";
+word += Hangman("Ingliz alifbosidagi 2-harf ", "B");
+
+word += Hangman("Nolga o'xshash harf ", "o");
+
+word += Hangman("Ingliz tilida qanday 2 ta bir xil harf kelsa \'u\' shaklida o'qiladi ", "o");
+
+word += Hangman("Dengizda suza oladigan qurilma nomi qanday harfdan boshlanadi", "k");
+
+Console.WriteLine(word + " so'zini muvafaqiyatli topdingiz!");
+
+string Hangman(string question, string answer)
+{
+    string userInput;
+    do
+    {
+        Console.Clear();
+        Status(correctAnswer, word);
+        Console.WriteLine(question);
+        Console.Write("=>"); userInput = Console.ReadLine();
+        if(userInput.ToLower() != answer.ToLower())
+        {
+            Console.WriteLine("Sizning javobingiz xato, qaytadan urinib ko'ring!\n");
+        }
+    }while(userInput.ToLower() != answer.ToLower());
+    Console.Clear();
+    Console.WriteLine("Sizning javobingiz to'g'ri\n");
+    return answer;
+}
+
+void Status(string correctAnswer, string answer)
+{
+    Console.Clear();
+    Console.WriteLine("Sizning statusingiz : ");
+    Console.Write(answer);
+    for(int i = 0; i < correctAnswer.Length - answer.Length; i++)
+    {
+        Console.Write('-');
+    }
+    Console.WriteLine("\n");
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # HangmanGame
-Bu loyiha so'z o'yiniga o'xshash loyiha hisoblanadi
+Bu loyiha so'z o'yiniga o'xshash loyiha hisoblanadi  
+Bu o'yin sodda o'yin hisoblanib, hozircha fixed ma'lumotlar uchun ishlaydi.  
+Keyinchalik uni mukammallashtirib foydalanuvchi tomonidan qo'shimcha so'zlar uchun ham   
+ishlaydigan va uni xotiraga saqlab qo'yadigan qilsak bo'ladi.  
+
+![](rasm1.png "Dastur boshi") 
+Dastur boshlanish qismi.   
